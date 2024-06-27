73fd19b74e662c25fafbb48cb9243c1fd3788d5f
Dmytro <dmytro.kupets@gmail.com>
2024-06-27 12:17:26 +0300

    feat: delete session express and mailer

--- src/controller/auth.js
+++ src/controller/auth.js
@@ -3,17 +3,8 @@ const db = require("../sequelize/models/index");
 
 const crypto = require('crypto');
 const { validationResult } = require("express-validator");
-const nodemailer = require("nodemailer");
-const sendgridTransport = require("nodemailer-sendgrid-transport");
 const bcrypt = require("bcrypt");
 
-const transporter = nodemailer.createTransport(
-  sendgridTransport({
-    auth: {
-      api_key: process.env.SENDGRID_API_KEY,
-    },
-  })
-);
 
 exports.getLogin = (req, res) => {
   let message = req.flash("error");
@@ -161,56 +152,3 @@ exports.postLogout = (req, res) => {
     res.redirect("/");
   });
 };
-exports.getReset = (req, res, next) => {
-  let message = req.flash("error");
-  if (message.length > 0) {
-    message = message[0];
-  } else {
-    message = null;
-  }
-  res.render("reset", {
-    pageTitle: "Reset password",
-    errorMessage: message,
-    oldInput: {
-      email: "",
-      password: "",
-      confirmPassword: "",
-    },
-    validationErrors: [],
-  });
-}
-exports.postReset = (req, res, next) => {
-  crypto.randomBytes(32, (err, buffer) => {
-    if (err) {
-      console.log(err);
-      return res.redirect('/reset');
-    }
-    const token = buffer.toString('hex');
-    User.findOne({ email: req.body.email })
-      .then(user => {
-        if (!user) {
-          req.flash('error', 'No account with that email found.');
-          return res.redirect('/reset');
-        }
-        user.resetToken = token;
-        user.resetTokenExpiration = Date.now() + 3600000;
-        return user.save();
-      })
-      .then(result => {
-        res.redirect('/');
-        transporter.sendMail({
-          to: req.body.email,
-          from: 'shop@node-complete.com',
-          subject: 'Password reset',
-          html: `
-            <p>You requested a password reset</p>
-            <p>Click this <a href="http://localhost:3000/reset/${token}">link</a> to set a new password.</p>
-          `
-        });
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  });
-};
-
--- src/routes/weather.js
+++ src/routes/weather.js
@@ -7,7 +7,7 @@ const isAuth = require('../middleware/is-auth');
 
 router.get('/', homeController.getHomePage);
 router.get('/locations', weatherController.getLocations);
-router.post('/locations', isAuth, weatherController.getLocations);
+router.post('/locations', weatherController.getLocations);
 router.get('/weather/:lat/:lon', weatherController.getWeather);
 
 module.exports = router;
