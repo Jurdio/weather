1fed53e4c9155bae82090e9c5cb4f41b657d1041
Dmytro <dmytro.kupets@gmail.com>
2024-06-27 12:25:17 +0300

    chore: delete auto session

--- src/app.js
+++ src/app.js
@@ -1,20 +1,14 @@
 const path = require('path');
 const express = require('express');
-const session = require('express-session');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 
 const weatherRouter = require('./routes/weather');
 const authRouter = require('./routes/auth');
 
 const flash = require('connect-flash');
 
-const { sequelize } = require('./sequelize/models/index');
-
 const app = express();
-const store = new SequelizeStore({
-  db: sequelize,
-});
-store.sync();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -22,24 +16,13 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'view'));
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(
-  session({
-    secret: 'my secret',
-    resave: false,
-    saveUninitialized: false,
-    store: store
-  })
-);
 app.use(flash());
-app.use((req, res, next) => {
-  res.locals.isAuthenticated = req.session.isLoggedIn;
-  next();
-});
+
 app.use(authRouter);
 app.use(weatherRouter);
 
 
-// Test DB connection
+
 sequelize.authenticate()
   .then(() => {
     console.log('Connection has been established successfully.');
